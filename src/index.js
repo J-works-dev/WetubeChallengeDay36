ccd1d9799bfef6632588c208ea0d9b7d043a60a1
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2020-12-21 21:50:08 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import "./styles.css";
 
 const API_URL = "https://api.coinpaprika.com/v1/tickers";
+
+const progress = document.querySelector(".progress");
+
+progress.innerHTML = "Finding coins.....";
+// let coins = {};
+
+fetch(API_URL)
+  .then((response) => response.json())
+  .then((data) => {
+    const { name, quotes } = data;
+  })
+  .then((data) => {
+    const { USD } = data;
+  })
+  .then((data) => {
+    const { price } = data;
+    progress.innerHTML = `Name: ${name}, Price: ${price}`;
+  })
+  .catch((e) => {
+    console.log(e);
+    progress.innerHTML = "Can't find coins!";
+  });
